569af7d361e3a5e9d65a607ddbb0a6766100e108
Harry <wigglespottery@yahoo.com>
2020-04-08 16:47:39 +0100

    2.2

--- task2_sNeuron.m
+++ task2_sNeuron.m
@@ -7,5 +7,18 @@ function [Y] = task2_sNeuron(W, X)
 %  W : (D+1)-by-1 vector of weights (double)
 % Output:
 %  Y : N-by-1 vector of output (double)
+[N,D] = size(X);
+[Dw,~] = size(W);
 
+Y = zeros(N,1);
+w = W(2:Dw)';
+wZero = W(1);
+
+a = task2_hNeuron(W,X);
+
+for i = 1:N
+    aN = a(i);
+    
+    Y(i)= 1/(1+exp(-aN));
+end
 end
