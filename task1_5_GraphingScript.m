cd1b325df11ee7bf06e6e6e1f904bdb986347bd6
Harry <wigglespottery@yahoo.com>
2020-04-12 18:40:47 +0100

    Save that jazz 1.2

--- task1_5_GraphingScript.m
+++ task1_5_GraphingScript.m
@@ -1,12 +1,25 @@
-hold off;
-
+%hold off;
 load('dset.mat');
+[N,D] = size(X);
 
 cK = 1;
 kF = 5;
 
+accuracies = zeros(D,2);
+ind = 1;
+
 for i = -20:3
     epsilon = 1*10^(i);
     
+    task1_mgc_cv(X,Y_species,cK,epsilon,kF);
+    load('t1_mgc_5cv6_ck1_CM.mat');
     
+    accuracies(ind,1) = epsilon;
+    accuracies(ind,2) = mean(diag(CM));
+    ind = ind + 1;
 end
+
+x = accuracies(:,1);
+y = accuracies(:,2);
+
+plot
