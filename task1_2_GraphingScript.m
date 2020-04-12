085a8ba76f5ec677cdd6cdbf37c571fdba8bd2cf
Harry <wigglespottery@yahoo.com>
2020-04-12 16:14:58 +0100

    1.2/3

--- task1_2_GraphingScript.m
+++ task1_2_GraphingScript.m
@@ -1,8 +1,13 @@
-
+load('t1_R.mat');
+figure('Renderer', 'painters', 'Position', [10 10 900 600])
 
 for i = 1:24
     colour = [0.03 0.144 0.255];
     subplot(6,4,i)
+        
+    ax= gca;
+    ax.FontSize = 3;
+    
     xlim([0 24]);
     ylim([-1 1]);
     x = 1:24;
@@ -19,17 +24,15 @@ for i = 1:24
     hold on;
     str = sprintf('Corr. of feature %d',i);
     title(str,'FontSize',5);
-    hold on; 
-    
-    xlabel = 'Feature #';
-    ylabel = 'Corr.';
     hold on;
-   
-    ax= gca;
+    
+    ax = gca;
     ax.FontSize = 4;
+    
     xticks([1 4 8 12 16 20 24]);
     hold on;
     yticks([-1, -0.5, 0 ,0.5, 1]);
 end
+
     
     
--- task1_3_GraphingScript.m
+++ task1_3_GraphingScript.m
@@ -1,8 +1,14 @@
-
 load('t1_Cumvar.mat');
 [D,~] = size(Cumvar);
 
 y = Cumvar./Cumvar(D);
 x = (1:24)';
 
-scatter(x,y,'+');
+
+figure;
+s = scatter(x,y,'+');
+xlabel('Number of Components')
+ylabel('Proportion of Variance')
+
+
+title({'Cumulative Variance'});
