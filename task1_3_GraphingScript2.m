a56511209f4c733ce507aac790b7c1f76f8a57c6
Harry <wigglespottery@yahoo.com>
2020-04-12 17:52:24 +0100

    PCA

--- task1_3_GraphingScript2.m
+++ task1_3_GraphingScript2.m
@@ -1,3 +1,5 @@
+hold off;
+
 load('t1_EVecs.mat');
 load('t1_S.mat');
 load('dset.mat');
@@ -8,6 +10,7 @@ PC(:,1) = EVecs(:,1);
 PC(:,2) = EVecs(:,2);
 
 PC_X = X*PC;
+colours = parula(10);
 
 for c = 1:10
     indexes = find(Y_species == c);
@@ -18,10 +21,21 @@ for c = 1:10
     end
     PC_Xc = X_class*PC;
     
-    scatter(PC_Xc(:,1),PC_Xc(:,2),'+');
+    scatter(PC_Xc(:,1),PC_Xc(:,2),1,colours(c,:),'+');
     hold on;
-end
+end 
 
-scatter(PC_X(:,1),PC_X(:,2),'+');
+
+% scatter(PC_X(:,1),PC_X(:,2),'+');
 xlabel('1st Principal Component');
 ylabel('2nd Principal Component');
+%legend(list_species(1:10));
+cb = colorbar('eastoutside','Ticks',(1:10)./10,'TickLabels',list_species(1:10));
+cb.FontSize = 7;
+hold on;
+
+print('gcf','-dpdf');
+% cb.Ticks = 1:10;
+% hold on;
+% cb.TickLabels = list_species(1:10);
+% hold on;
