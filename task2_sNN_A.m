899a1e2539fe03f1baffaf5f2b69b58c31e7578f
Harry <wigglespottery@yahoo.com>
2020-04-13 17:22:28 +0100

    neurals :)))

--- task2_sNN_A.m
+++ task2_sNN_A.m
@@ -11,17 +11,16 @@ polyA =[1.89311,4.03806;
         2.13083,2.98602;
         2.59935,3.57138];
 weights = task2_find_hNN_A_weights(polyA); %weight vectors for layer 1
+weights = 10^8*weights;
 
 YneurOut1 = zeros(N,4);
 for i = 1:4 %1st hidden layer
-    neuronOUT = task2_sNeuron(weights(i,:),X);% >= 0.5;
+    neuronOUT = task2_sNeuron(weights(i,:),X);
     
     YneurOut1(:,i) = neuronOUT;
 end
-w = YneurOut1(:,1).*YneurOut1(:,2).*YneurOut1(:,3);
 
-weights = [0 w];
-Y = task2_sNeuron(weights,YneurOut1(:,4));
-% weights = [0 1];
-% Y = task2_sNeuron(weights,neurOUT)
+weights = [-1 0.25 0.25 0.25 0.251];
+weights = 10^8*weights;
+Y = task2_sNeuron(weights,YneurOut1);
 end
--- task2_sNN_AB.m
+++ task2_sNN_AB.m
@@ -11,15 +11,8 @@ function [Y] = task2_sNN_AB(X)
 Y_polyA = task2_sNN_A(X); %neural network classification of polyA
 Y_polyB = task2_sNN_B(X); %neural network classification of polyB
 
-YneurOut = cat(2, Y_polyA + 1-Y_polyB , Y_polyB-Y_polyA)
-%Classification: 1 = inside, 0 = outside
-
-Y = YneurOut(:,2)>YneurOut(:,1);
-
-%Class 1 = not A and B, so Y_polyA = 0 and Y_polyB = 1
-%Class 0 = A or not B, so Y_polyA = 1 or Y_polyB = 0
-% 
-% weights = [-1 -1 1];
-% 
-% Y  = find(max(task2_sNeuron(weights,YneurOut)))-1;
+weights = [-0.1 -1.01 1];
+weights = 10^8*weights;
+YneurOut = cat(2, Y_polyA, Y_polyB)
+Y = task2_sNeuron(weights,YneurOut);
 end
